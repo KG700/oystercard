be45b02fa773340e6b82d4f4c56cca5aa4d524c7
Orion Reed <orionreedt@gmail.com>
2020-02-26 19:04:36 +0000

    added journey history

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,47 +3,46 @@
 # load '~/Dropbox/makers_projects/makers_week_02/oystercard/lib/oystercard.rb'
 
 class Oystercard
-
   CARD_MAX = 90.00
   @max_limit_error = "The limit is £#{CARD_MAX}"
 
   CARD_MIN = 1.00
   @min_limit_error = "The minimum balance to travel is £#{CARD_MIN}"
 
-  attr_reader :balance, :in_journey_status, :entry_station
+  attr_reader :balance, :in_journey_status, :entry_station, :journeys
 
   def initialize
     @balance = 0.00
     @in_journey = false
     @entry_station = nil
+    @journeys = []
   end
 
   def top_up(value)
-    fail @max_limit_error if @balance + value > CARD_MAX
+    raise(@max_limit_error) if @balance + value > CARD_MAX
     @balance += value
   end
-
 
   def in_journey?
     @in_journey
   end
 
   def touch_in(station)
-    fail @min_limit_error if @balance < CARD_MIN
+    raise(@min_limit_error) if @balance < CARD_MIN
     @entry_station = station
     @in_journey = true
   end
 
-  def touch_out
+  def touch_out(station)
     deduct(1.00)
+    @journeys << { @entry_station => station }
     @entry_station = nil
     @in_journey = false
   end
 
   private
+
   def deduct(fare)
     @balance -= fare
   end
-
-
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,32 +1,36 @@
 require 'oystercard'
 
 describe Oystercard do
-
-  let(:station) { double("station",:name => "algate") }
+  let(:entry_double) { double("entry_double", name: "algate") }
+  let(:exit_double) { double("exit_double", name: "poo") }
 
   context '#attributes variables' do
     it "has balance" do
-      expect(subject.balance).to be (0.00)
+      expect(subject.balance).to(be(0.00))
     end
 
     it "has entry_station" do
       subject.instance_variable_defined?(:@entry_station)
     end
 
+    it "has journey history" do
+      subject.instance_variable_defined?(:@journeys)
+    end
+
     it "changes the balance variable" do
-      expect {subject.top_up(50.00)}.to change(subject, :balance)
+      expect { subject.top_up(50.00) }.to(change(subject, :balance))
     end
   end
 
   describe "#top_up" do
-    it {is_expected.to respond_to(:top_up).with(1).argument }
+    it { is_expected.to(respond_to(:top_up).with(1).argument) }
     it "adds value to balance" do
       subject.top_up(80.00)
-      expect(subject.balance).to eq (80.00)
+      expect(subject.balance).to(eq(80.00))
     end
 
     it "raises an error if top up brings balance over £90" do
-      expect{ subject.top_up(95.00) }.to raise_error(@max_limit_error)
+      expect { subject.top_up(95.00) }.to(raise_error(@max_limit_error))
     end
   end
 
@@ -36,16 +40,16 @@ describe Oystercard do
     end
 
     it "reduces balance by a given amount; 'fare'" do
-      subject.send(:deduct,5.50)
-      #subject.deduct(5.50)
-      expect(subject.balance).to eq (4.50)
+      subject.send(:deduct, 5.50)
+      # subject.deduct(5.50)
+      expect(subject.balance).to(eq(4.50))
     end
   end
 
   describe "#in_journey?" do
     it "returns 'false' or 'true' depending on current card status" do
-    # assume default status is 'false'??
-    expect(subject).not_to be_in_journey
+      # assume default status is 'false'??
+      expect(subject).not_to(be_in_journey)
     end
   end
 
@@ -55,22 +59,20 @@ describe Oystercard do
     end
 
     it "can touch-in" do
-      subject.touch_in(station)
-      expect(subject).to be_in_journey
+      subject.touch_in(entry_double)
+      expect(subject).to(be_in_journey)
     end
 
     it "raises an error if card has less than one pound at touch in" do
       subject.instance_variable_set(:@balance, 0.50)
-      expect{ subject.touch_in }.to raise_error(@min_limit_error)
+      expect { subject.touch_in }.to(raise_error(@min_limit_error))
     end
-
 
     it "updates the entry station" do
-      subject.touch_in(station)
-      expect(subject.instance_variable_get(:@entry_station).name).to eq "algate"
-      #expect(subject.entry_station.name).to eq "algate"
+      subject.touch_in(entry_double)
+      expect(subject.instance_variable_get(:@entry_station).name).to(eq("algate"))
+      # expect(subject.entry_station.name).to eq "algate"
     end
-
   end
 
   describe "#touch_out" do
@@ -79,23 +81,26 @@ describe Oystercard do
     end
 
     it "allows to touch out" do
-      subject.touch_in station
-      subject.touch_out
-      expect(subject).not_to be_in_journey
+      subject.touch_in(entry_double)
+      subject.touch_out(exit_double)
+      expect(subject).not_to(be_in_journey)
+    end
+
+    it "accepts exit station" do
+      expect(subject).to(respond_to(:touch_out).with(1).argument)
     end
 
     it "reduces balance by 1.00" do
-      expect {subject.touch_out}.to change{subject.balance}.by(-1.00)
+      expect { subject.touch_out(exit_double) }.to(change { subject.balance }.by(-1.00))
+    end
+
+    it "adds journey hash to journeys" do
+      expect { subject.touch_out(exit_double) }.to(change { subject.journeys.length }.by(1))
     end
 
     it "changes entry_station to nil" do
-      subject.touch_in(station)
-      expect { subject.touch_out }.to change{subject.entry_station}.to(nil)
+      subject.touch_in(entry_double)
+      expect { subject.touch_out(exit_double) }.to(change { subject.entry_station }.to(nil))
     end
-
-
   end
-
-
-
 end
